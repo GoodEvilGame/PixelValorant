e55159c791474729f797366183e709646e503ee0
Vinícius Silva <vivisec007@gmail.com>
2021-06-23 00:45:19 -0300

    Adição som de tiro e música de fundo

--- PixelValorant/Assets/Bullet.cs
+++ PixelValorant/Assets/Bullet.cs
@@ -2,8 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class Bullet : MonoBehaviour
-{
+{   
+    public AudioClip vandal;
+    AudioSource audioSource;
     public float speed = 40f;
     public Rigidbody2D rb;
     public string TagE;
@@ -11,6 +14,8 @@ public class Bullet : MonoBehaviour
 
     void Start()
     {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.PlayOneShot(vandal, 1.0F);
         rb.velocity =  new Vector2(Direction * speed, 0);
     }
 
